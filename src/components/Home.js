6655285f86ce87abcf586e22ce5ccc581fcef49a
Paul G <paulgallagher37@yahoo.co.uk>
2025-11-20 19:56:12 +0000

    adding initial layout

---
+++ src/components/Home.js
@@ -0,0 +1,14 @@
+import React from "react";
+import logo from "../images/logo-1.png"
+
+function Home() {
+    return (
+        <div>
+            <div className="home-logo">
+                <img src={logo} alt="logo" width="13%"/>
+            </div> 
+        </div>
+    )
+};
+
+export default Home;
---
+++ src/components/Navbar.js
@@ -0,0 +1,21 @@
+import React from "react";
+
+function Navbar() {
+    return (
+        <div className="navbar">
+            <ul className="navbar-ul">
+                <li>
+                    <p>Home</p>
+                </li>
+                <li>
+                    <p>Quote</p>
+                </li>
+                <li>
+                    <p>Contact</p>
+                </li>
+            </ul>
+        </div>
+    )
+};
+
+export default Navbar;
